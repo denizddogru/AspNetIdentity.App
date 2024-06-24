a9093eadfc0cea2c45fa87370091a2194e518148
Deniz Doğru <deniz.dogru@venhancer.com>
2024-06-24 15:09:21 +0300

    Chapter 2: IdentityDbContext, database(identity) created. Tables including Role and User fields coming from the Identity library is added through the tables with migrations

--- AspNetIdentity.App/Program.cs
+++ AspNetIdentity.App/Program.cs
@@ -1,7 +1,28 @@
+using AspNetIdentity.App.Models;
+using AspNetIdentityApp.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
+});
+
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<AppDbContext>();
+
+
+
+
+
+
+
 
 var app = builder.Build();
 
