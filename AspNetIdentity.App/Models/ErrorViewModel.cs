2b7bc404e6975a5b3f10f6bddc2c51f3996d1074
Deniz Doğru <deniz.dogru@venhancer.com>
2024-06-24 14:08:25 +0300

    Add project files.

---
+++ AspNetIdentity.App/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspNetIdentity.App.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
