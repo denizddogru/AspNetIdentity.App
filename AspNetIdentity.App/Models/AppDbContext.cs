b6565ecd67373bc5f7ada65d9a6b1ac736b8ac04
Deniz Doğru <deniz.dogru@venhancer.com>
2024-06-24 14:39:09 +0300

    Chapter 2: Creating the IdentityDbContext structure

---
+++ AspNetIdentity.App/Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AspNetIdentity.App.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetIdentityApp.Models;
+
+public class AppDbContext : IdentityDbContext<AppUser, AppRole, string>
+{
+
+
+
+    // base : miras aldığımız sınıfı consturctora gönderir
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+}
